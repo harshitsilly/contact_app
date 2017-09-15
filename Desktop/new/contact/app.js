1de8d38bae36eb0615bddbde915c56aa4ef09904
himanshu <himanshu@sap.com>
2017-09-15 12:48:46 +0530

    first commit

---
+++ Desktop/new/contact/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+//const api = require('./server/routes/api');
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+const port = process.env.PORT || '3001' ;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running'));
